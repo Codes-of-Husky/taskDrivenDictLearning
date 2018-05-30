22b98c35195d0642b35ba2634283121f28ac1747
Sheng Lundquist <slundqui007@gmail.com>
2018-05-30 16:02:26 -0600

    Initial commit with mnist

---
+++ data/mnistData.py
@@ -0,0 +1,51 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+
+"""
+An object that handles data input
+"""
+class mnistData(object):
+    raw_image_shape = (28, 28, 1)
+    inputShape = (784,)
+    numClasses = 10
+
+    #TODO set random seed
+    def __init__(self, path, flatten=True):
+        self.mnist = input_data.read_data_sets(path, one_hot=False)
+
+        self.num_train_examples = self.mnist.train.num_examples
+        self.num_test_examples = self.mnist.test.num_examples
+        self.flatten=flatten
+
+        self.train_images = self.mnist.train.images
+        self.train_labels = self.mnist.train.labels
+        self.test_images = self.mnist.test.images
+        self.test_labels = self.mnist.test.labels
+
+    def getData(self, numExample):
+        images, labels = self.mnist.train.next_batch(numExample)
+        if(not self.flatten):
+            #Reshape into y, x, f
+            images = np.reshape(images, (numExample,) + self.raw_image_shape)
+        return (images, labels)
+
+    def getTestData(self):
+        test_images = self.test_images
+
+        if(not self.flatten):
+            #Reshape into y, x, f
+            test_images = np.reshape(test_images,
+                    (self.num_test_examples,) + self.raw_image_shape)
+
+        return (test_images, self.test_labels)
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    path = "/home/slundquist/mountData/datasets/mnist"
+    obj = mnistData(path, flatten=False)
+    (train_data, train_gt) = obj.getData(10)
+
+    plt.figure()
+    r_data = train_data[0, :, :, 0]
+    plt.imshow(r_data, cmap="gray")
+    plt.show()
---
+++ mnistRun.py
@@ -0,0 +1,1 @@
+from mnistData import mnistData
